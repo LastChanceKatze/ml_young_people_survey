ab7e821945e5a474e5e2bd7920d7d8333498f49c
Milica Todorovic <last.chance.katze@elfak.rs>
2021-02-02 23:37:42 +0100

    preprocess data

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pandas as pd
+import preprocess as pp
 
 desired_width = 500
 pd.set_option('display.width', desired_width)
 pd.set_option('display.max_columns', 20)
 
-
+# load data
 data = pd.read_csv("dataset/responses.csv")
 print(data.head(2))
 
+# select movie columns
 movie_columns = ["Movies", "Horror", "Thriller", "Comedy", "Romantic",
                  "Sci-fi", "War", "Fantasy/Fairy tales", "Animated",
                  "Documentary", "Western", "Action", "Age",
@@ -16,3 +18,14 @@ movie_columns = ["Movies", "Horror", "Thriller", "Comedy", "Romantic",
 
 data_movie = data[movie_columns]
 print(data_movie.info())
+
+# check missing vals
+# data contains missing vals
+pp.check_missing_vals(data_movie)
+
+# impute data
+data_movie = pp.impute(data_movie)
+pp.check_missing_vals(data_movie)
+
+# describe data
+print(data_movie.describe())
---
+++ preprocess.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.impute import SimpleImputer
+
+desired_width = 500
+pd.set_option('display.width', desired_width)
+pd.set_option('display.max_columns', 20)
+
+
+# check missing values
+def check_missing_vals(data):
+    # check for missing values
+    missing_vals = data.isna().sum()
+    print("Missing values: \n" + str(missing_vals))
+    return missing_vals.sum() != 0
+
+
+def impute(data):
+    num_data = data[data.columns[0:14]]
+    im_num = SimpleImputer(missing_values=np.nan, strategy="mean", copy=False)
+    im_num.fit_transform(num_data)
+
+    cat_data = data[data.columns[14:]]
+    im_cat = SimpleImputer(missing_values=np.nan, strategy="most_frequent", copy=False)
+    im_cat.fit_transform(cat_data)
+
+    return pd.concat([num_data, cat_data], axis=1)
+
+
+# encode labels
+def encode_labels(data):
+    encode_quality = LabelEncoder()
+    data['quality'] = encode_quality.fit_transform(data['quality'])
+    return data
+
+
+# feature scaling
+def scale_features(x):
+    scaler = StandardScaler()
+    x = scaler.fit_transform(x)
+    return x
+
+
+
